a01cdbd5e8b6149ac53a6183bd415b79e4951aec
NinFreakinJa <60324136+NinFreakinJa@users.noreply.github.com>
2022-02-24 00:31:11 -0500

    Update DATProcess.py

--- Scripts/DATProcess.py
+++ Scripts/DATProcess.py
@@ -46,7 +46,7 @@ def processDAT(filepath,source_path,output_path,archive_path):
     print(threading.current_thread().name,"- Processing "+filepath)
     with open(output_path+"\\"+source_path.split("\\")[-1]+filepath.replace(source_path,""),"w") as file:
         file.write(str(read_file(filepath)))
-    shutil.move(filepath,archive_path+"\\"+source_path.split("\\")[-1]+filepath.replace(source_path,""))
+    shutil.move(filepath,archive_path+"\\"+source_path.split("\\")[-1]+filepath.replace(source_path,"").replace(".dat",".txt"))
     print(threading.current_thread().name,"- Finished Processing "+filepath)
 
 if __name__ == "__main__":
