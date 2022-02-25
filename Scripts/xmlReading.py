bdf6683908c768c0cbac694ca476a812bb93b9bd
kyragovan <43038822+kyragovan@users.noreply.github.com>
2022-02-25 14:33:52 -0500

    Add files via upload

---
+++ Scripts/xmlReading.py
@@ -0,0 +1,21 @@
+
+import xmltodict
+import pprint
+
+def xml_to_dict(fileName):
+    with open(fileName, 'r', encoding='utf-8') as file:
+        my_xml = file.read()
+
+    my_dict = xmltodict.parse(my_xml)
+
+    pprint.pprint(my_dict, indent=2)
+
+
+    try:
+        newDict = open('xmlDictionary.txt', 'wt')
+        newDict.write(str(my_dict))
+        newDict.close()
+        
+    except:
+         print("Unable to write to file")
+    return[my_dict]
