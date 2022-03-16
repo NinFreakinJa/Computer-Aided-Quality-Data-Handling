df99c74fd230a511ef8874689c994a2457cdc4e6
NinFreakinJa <60324136+NinFreakinJa@users.noreply.github.com>
2022-03-16 00:07:46 -0400

    Update DATProcess.py

--- Scripts/DATProcess.py
+++ Scripts/DATProcess.py
@@ -33,8 +33,8 @@ def read_file(filename):
                     stack.append(current[line.strip()])
                     current=stack[-1]
                 indentCount=count
-            elif("Date" in line):
-                current["Date"]=":".join(line.split(":")[1:]).strip()
+            elif("; Date:" in line):
+                current["Date"]=format_date(line)
             elif("#Index" in line):
                 read=True
     return complete
@@ -45,13 +45,18 @@ def print_dict(input,indent):
         if(type(input[i])==dict):
             print_dict(input[i],indent+1)
 
+def format_date(datetime):
+    datetime=":".join(datetime.split(":")[1:]).strip()
+    datetime=datetime.split(" ")
+    return datetime[0]+"."+datetime[1].replace("Jan","01").replace("Feb","02").replace("Mar","03").replace("Apr","04").replace("May","05").replace("Jun","06").replace("Jul","07").replace("Aug","08").replace("Sept","09").replace("Sep","09").replace("Oct","10").replace("Nov","11").replace("Dec","12")+"."+datetime[2]+"/"+datetime[4]
+
 def processDAT(filepath,source_path,output_path,archive_path):
     if(os.path.exists(filepath)):
         print(threading.current_thread().name,"- Processing "+filepath)
         with open(filepath, 'r+') as file:
             os.fsync(file)
-        with open(output_path+"\\"+source_path.split("\\")[-1]+filepath.replace(source_path,"").replace(".dat",".txt"),"wt") as file:
-            pprint.pprint(read_file(filepath), stream=file)
+        with open(output_path+"\\"+source_path.split("\\")[-1]+filepath.replace(source_path,"").replace(".dat",".dfq"),"wt") as file:
+            #pprint.pprint(read_file(filepath), stream=file)
             os.fsync(file)
         shutil.move(filepath,archive_path+"\\"+source_path.split("\\")[-1]+filepath.replace(source_path,""))
         print(threading.current_thread().name,"- Finished Processing "+filepath)
