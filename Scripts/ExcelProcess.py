ab8747d6a020ae97445eb60d5c985e88b2ef5796
Bonvillain <98856281+Bonvillain@users.noreply.github.com>
2022-03-17 15:13:31 -0400

    Update ExcelProcess.py

--- Scripts/ExcelProcess.py
+++ Scripts/ExcelProcess.py
@@ -3,6 +3,7 @@ import threading
 import shutil
 import os
 
+
 def read_file(fileName):
     try:
         #Determine Excel Type
@@ -11,62 +12,71 @@ def read_file(fileName):
 
         if numSheets == 1:
             repo = pd.read_excel(xls, 'Sum Report')
-            repo.dropna(how='all', axis=1, inplace=True)
-            repo.dropna(how='all', axis=0, inplace=True)
             return convertNacXls(repo)
         else:
             proto = pd.read_excel(xls, 'Protokoll_Intern')
-            proto.dropna(how='all', axis=1, inplace=True)
-            proto.dropna(how='all', axis=0, inplace=True)
             return proto.to_string()
     except:
         return ""
 
-def convertNacXls(xls):
-#Prepare dataframe for conversion    
+def convertNacXls(xls):   
     
     #Split header and data into two dataframes
-    header = xls.iloc[:10, :28]
-    data = xls.iloc[10:18,:28]
+    header = xls.iloc[:10, :]
+    data = xls.iloc[10:,:]
     
+    #Create null test dataframe
+    dfDimension = data.isna()
+    
+    #Count Rows
+    rc = 0
+    while dfDimension.iloc[rc,0] == False:
+        rc+= 1 
+    
+    # Count Columns
+    cc = 0
+    while dfDimension.iloc[0,cc] == False  and dfDimension.iloc[2,cc] == False:
+        cc+= 1
+
     #Define DFQ string
     dfq = ""
 
     #Add Defenite Header Info (Date/Time, Batch Name, # Characterisitics)
-    dfq += "K0004 " + str(header.iloc[1,10]) + "\n"
+    dfq += "K0004 " + str(header.iloc[1,10].strftime("%d.%m.%Y")) + "/" + str(header.iloc[1,13]) + "\n"
     dfq += "K0006 " + str(header.iloc[1,1]) + "\n"
-    dfq += "K0100 " + str(28) + "\n"
+    dfq += "K0100 " + str(cc) + "\n"
     dfq += "K1001/1 " + str(data.iloc[2,0]) +"\n"
     
 
     #Create loop to iterate df
-    for c in range (1,28):
+    for c in range (1,cc):
         #Determine characteristic name/value
         r = 0
-        dfq += "K0001/" + str(c+1) + " " + str(data.iloc[r+2,c]) + "\n"
+        if dfDimension.iloc[r+2,c] == False:
+            dfq += "K0001/" + str(c+1) + " " + str(data.iloc[r+2,c]) + "\n"
         
-        dfq += "K2002/" + str(c+1) + " " + str(data.iloc[r,c]) + "\n"
+            dfq += "K2002/" + str(c+1) + " " + str(data.iloc[r,c]) + "\n"
         
-        #Determine decimal places
-        if c == 1:
-            dfq += "K2022/" + str(c+1) + " " + str(0) + "\n"
-        elif c == 4 or c == 23 or c == 24 or c== 27:
-            dfq += "K2022/" + str(c+1) + " " + str(2) + "\n" 
-        else:
-            dfq += "K2022/" + str(c+1) + " " + str(3) + "\n" 
+            #Determine decimal places
+            if c == 1:
+                dfq += "K2022/" + str(c+1) + " " + str(0) + "\n"
+            elif c == 4 or c == 23 or c == 24 or c== 27:
+                dfq += "K2022/" + str(c+1) + " " + str(2) + "\n" 
+            else:
+                dfq += "K2022/" + str(c+1) + " " + str(3) + "\n" 
 
         #Determine unit of measurement
-        if(str(data.iloc[r+1,c])!="[-]"):
-            dfq += "K2142/" + str(c+1) + str(data.iloc[r+1,c]).strip("[]")
+            if(str(data.iloc[r+1,c])!="[-]"):
+                dfq += "K2142/" + str(c+1) + " " + str(data.iloc[r+1,c]).strip("[]") + "\n"
 
     #For loop to dump remain
-    for i in range (2,8):
-        for j in range (0,28):
-            if j != 27:
+    for i in range (2,rc):
+        for j in range (0,cc):
+            if j != cc-1:
                 dfq += str(data.iloc[i,j]) + chr(0x000f)
             else: 
                 dfq += str(data.iloc[i,j]) + chr(0x000f) + "\n"  
-
+    
     return dfq    
       
 
